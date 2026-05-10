166bbeac63cbe843dd5d99c7c33748e921cca5a8
RayenZouabi1 <zouabi.rayen@esprit.tn>
2026-05-10 13:34:24 +0100

    Fix uninitialized roles property and ensure all users have valid roles data

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -93,7 +93,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        $roles = $this->roles;
+        $roles = $this->roles ?? [];
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
